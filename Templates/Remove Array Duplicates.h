7762847233356431ec2e00870b312ca948574f9a
Muhammad Masab Hammad <136446395+mmasabalvi@users.noreply.github.com>
2024-01-29 22:21:11 +0500

    Update Remove Array Duplicates.h

--- Templates/Remove Array Duplicates.h
+++ Templates/Remove Array Duplicates.h
@@ -25,5 +25,36 @@ T* removeDuplicates(T array[], int size, int &newSize)
         }
     }
 
+    //------------------------------------------//
+//           Duplicates Removal             //
+//------------------------------------------//
+
+/*TEST(RemoveDuplicatesTest, IntArrayTest) {
+    int intArray[] = { 1, 2, 3, 4, 2, 5, 6, 3, 7, 8, 1 };
+    int uniqueIntCount;
+    int* uniqueIntArray = removeDuplicates(intArray, 11, uniqueIntCount);
+
+    int expectedUniqueIntArray[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+    int expectedUniqueIntCount = 8;
+
+    ASSERT_EQ(uniqueIntCount, expectedUniqueIntCount);
+
+    delete[] uniqueIntArray;
+}
+
+TEST(RemoveDuplicatesTest, CharArrayTest) {
+    char charArray[] = { 'a', 'b', 'c', 'b', 'd', 'e', 'a' };
+    int uniqueCharCount;
+    char* uniqueCharArray = removeDuplicates(charArray, 7, uniqueCharCount);
+
+    char expectedUniqueCharArray[] = { 'a', 'b', 'c', 'd', 'e' };
+    int expectedUniqueCharCount = 5;
+
+    ASSERT_EQ(uniqueCharCount, expectedUniqueCharCount);
+
+
+    delete[] uniqueCharArray;
+}*/
+
     return arr2;
 }
